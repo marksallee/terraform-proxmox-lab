34e095d51ccf5e67faf079684ea8f854ee47f894
Mark Sallee <1127062+marksallee@users.noreply.github.com>
2026-07-23 21:00:08 -0600

    Provision first terraform managed ubuntu LXC on Proxmox

---
+++ terraform/main.tf
@@ -0,0 +1,50 @@
+resource "proxmox_virtual_environment_container" "ubuntu" {
+
+  tags = [
+    "terraform",
+    "lab",
+    "github"
+  ]
+
+  node_name = var.proxmox_node
+  vm_id     = var.container_id
+
+  unprivileged = true
+
+  features {
+    nesting = true
+  }
+
+  initialization {
+    hostname = var.container_name
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+  }
+
+  network_interface {
+    name     = "veth0"
+    firewall = true
+  }
+
+  disk {
+    datastore_id = var.storage_pool
+    size         = 8
+  }
+
+  operating_system {
+    template_file_id = var.container_template
+    type             = "ubuntu"
+  }
+
+  cpu {
+    cores = 1
+  }
+
+  memory {
+    dedicated = 1024
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,3 +14,31 @@ variable "proxmox_insecure" {
   type        = bool
   default     = true
 }
+
+variable "proxmox_node" {
+  description = "Proxmox node name"
+  type        = string
+  default     = "proxmox"
+}
+
+variable "storage_pool" {
+  description = "LVM-Thin storage pool"
+  type        = string
+  default     = "lab-storage"
+}
+
+variable "container_template" {
+  description = "Ubuntu LXC template"
+  type        = string
+  default     = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+}
+
+variable "container_id" {
+  type    = number
+  default = 101
+}
+
+variable "container_name" {
+  type    = string
+  default = "web01"
+}
