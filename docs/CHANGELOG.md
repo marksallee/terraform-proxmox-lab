f31cb96a162b3e08be93f52ab06ae47a7340db10
Mark Sallee <1127062+marksallee@users.noreply.github.com>
2026-07-29 18:20:04 -0600

    Refactor Proxmox LXC deployment into reusable Terraform module

---
+++ docs/CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+## v0.1.0
+- Initial project structure
+- Added Terraform provider
+- Configured Proxmox authentication
+
+## v0.2.0
+- First Terraform-managed LXC container
+
+## v0.3.0
+- Added second container
+- Parameterized configuration
--- terraform/main.tf
+++ terraform/main.tf
@@ -13,3 +13,34 @@ module "web01" {
   memory    = 1024
   disk_size = 8
 }
+
+module "web02" {
+  source = "./modules/lxc-container"
+
+  node_name          = var.proxmox_node
+  vm_id              = 102
+  hostname           = "web02"
+  storage_pool       = var.storage_pool
+  container_template = var.container_template
+  container_ip       = "192.168.4.102/22"
+  gateway            = var.gateway
+
+  cores     = 1
+  memory    = 1024
+  disk_size = 8
+}
+
+module "postgres01" {
+  source    = "./modules/lxc-container"
+  node_name = var.proxmox_node
+  hostname  = "postgres01"
+  vm_id     = 110
+  container_ip = "192.168.4.110/22"
+  storage_pool  = var.storage_pool
+  container_template = var.container_template
+  gateway            = var.gateway
+
+  memory = 4096
+  cores = 2
+  disk_size = 20
+}
