390ad81c7191ab7e57f6be97a8d92159253c62f8
Mark Sallee <1127062+marksallee@users.noreply.github.com>
2026-08-24 21:39:11 -0600

    Use 10.42.0.0/24 for lab containers

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,9 @@ module "web01" {
   hostname           = "web01"
   storage_pool       = var.storage_pool
   container_template = var.container_template
-  container_ip       = "192.168.4.101/22"
+  container_ip       = "10.42.0.101/24"
   gateway            = var.gateway
- # ssh_public_key = file("~/.ssh/id_ed25519.pub") 
+  # ssh_public_key = file("~/.ssh/id_ed25519.pub")
 
   cores     = 1
   memory    = 1024
@@ -23,9 +23,9 @@ module "web02" {
   hostname           = "web02"
   storage_pool       = var.storage_pool
   container_template = var.container_template
-  container_ip       = "192.168.4.102/22"
+  container_ip       = "10.42.0.102/24"
   gateway            = var.gateway
- # ssh_public_key = file("~/.ssh/id_ed25519.pub")
+  # ssh_public_key = file("~/.ssh/id_ed25519.pub")
 
   cores     = 1
   memory    = 1024
@@ -33,17 +33,18 @@ module "web02" {
 }
 
 module "postgres01" {
-  source    = "./modules/lxc-container"
-  node_name = var.proxmox_node
-  hostname  = "postgres01"
-  vm_id     = 110
-  container_ip = "192.168.4.110/22"
-  storage_pool  = var.storage_pool
+  source = "./modules/lxc-container"
+
+  node_name          = var.proxmox_node
+  hostname           = "postgres01"
+  vm_id              = 110
+  container_ip       = "10.42.0.110/24"
+  storage_pool       = var.storage_pool
   container_template = var.container_template
   gateway            = var.gateway
- # ssh_public_key = file("~/.ssh/id_ed25519.pub") 
+  # ssh_public_key = file("~/.ssh/id_ed25519.pub")
 
-  memory = 4096
-  cores = 2
+  memory    = 4096
+  cores     = 2
   disk_size = 20
 }
