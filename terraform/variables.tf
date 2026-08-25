6b9f76a0c46a662a4b2bf6db627d43d5bb0e9d88
Mark Sallee <1127062+marksallee@users.noreply.github.com>
2026-08-24 21:39:37 -0600

    Update lab network defaults to 10.42.0.0/24

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -46,11 +46,11 @@ variable "container_name" {
 variable "container_ip" {
   description = "Container IPv4 address with CIDR"
   type        = string
-  default     = "192.168.4.101/22"
+  default     = "10.42.0.101/24"
 }
 
 variable "gateway" {
   description = "Default gateway"
   type        = string
-  default     = "192.168.4.1"
+  default     = "10.42.0.1"
 }
